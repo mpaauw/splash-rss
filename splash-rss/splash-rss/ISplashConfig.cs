dc2b7ba2b2f26e649b4c4474370f31f333994c2b
mpaauw <matt.paauw@gmail.com>
2017-10-30 18:23:52 -0700

    Added interface for SplashConfig class.

---
+++ splash-rss/splash-rss/ISplashConfig.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace splash_rss
+{
+    interface ISplashConfig
+    {
+        Dictionary<string, string> GetCommands();
+
+        void AddCommand(string key, string value);
+
+        void RemoveCommand(string key);
+    }
+}
