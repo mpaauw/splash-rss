29ce1f3eee31e8ba3352a4e267c74e6ef150592d
mpaauw <matt.paauw@gmail.com>
2017-11-02 18:19:39 -0700

    Added MemoryCache helper interface/class.

---
+++ splash-rss/splash-rss/ICache.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Caching;
+
+namespace splash_rss
+{
+    public interface ICache
+    {
+        void Insert(string key, object item);
+
+        bool Contains(string key);
+
+        object Get(string key);
+    }
+}
