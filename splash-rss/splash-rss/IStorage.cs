ee364de948bdac7838cabf830c42db06918576e9
mpaauw <matt.paauw@gmail.com>
2017-11-03 16:36:49 -0700

    Added documentation comments to IStorage interface.

--- splash-rss/splash-rss/IStorage.cs
+++ splash-rss/splash-rss/IStorage.cs
@@ -8,12 +8,26 @@ namespace splash_rss
 {
     public interface IStorage
     {
+        /// <summary>
+        /// Loads objects from JSON storage file and serializes them into readable/writable code objects.
+        /// </summary>
         void LoadStorage();
 
+        /// <summary>
+        /// Writes all code objects currently placed in storage into local data file using JSON serialization.
+        /// </summary>
         void SaveStorage();
 
+        /// <summary>
+        /// Retrieves all storage data.
+        /// </summary>
+        /// <returns>List of Feed objects.</returns>
         List<Feed> GetStorage();
 
+        /// <summary>
+        /// Inserts a new Feed item to local storage.
+        /// </summary>
+        /// <param name="item">Feed item to be inserted.</param>
         void AddStorageItem(Feed item);
     }
 }
