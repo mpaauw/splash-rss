ce9f1b307cda476401418edfcf6ca33061e34e1f
mpaauw <matt.paauw@gmail.com>
2017-11-02 17:49:55 -0700

    Added a class to represent a Feed object.

---
+++ splash-rss/splash-rss/Feed.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace splash_rss
+{
+    public class Feed
+    {
+        public string name;
+
+        public string endpoint;
+    }
+}
