bc6cfeabe33b17e7b468ebf5a30fe7598b869edd
Matt <matt.paauw@gmail.com>
2017-11-03 13:39:55 -0700

    Added new Storage interface and class, which utilizes a local JSON storage file to manage the storage and retrieval of Feed objects.

---
+++ splash-rss/splash-rss/IStorage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace splash_rss
+{
+    public interface IStorage
+    {
+        void LoadStorage();
+
+        List<Feed> GetStorage();
+
+        void AddStorageItem(Feed item);
+    }
+}
---
+++ splash-rss/splash-rss/Storage.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+using System.IO;
+
+namespace splash_rss
+{
+    public class Storage : IStorage
+    {
+        private List<Feed> feeds;
+
+        private JavaScriptSerializer serializer;
+
+        private const string dataFileName = "data.json";
+
+        public Storage()
+        {
+            feeds = new List<Feed>();
+            serializer = new JavaScriptSerializer();
+            LoadStorage();
+        }
+
+        public void LoadStorage()
+        {
+            using(StreamReader reader = new StreamReader(dataFileName))
+            {
+                string json = reader.ReadToEnd();
+                feeds = serializer.Deserialize<List<Feed>>(json);
+            }
+        }
+
+        public List<Feed> GetStorage()
+        {
+            return feeds;
+        }
+
+        public void AddStorageItem(Feed item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
