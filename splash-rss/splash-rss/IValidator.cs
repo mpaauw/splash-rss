47c85f96160ebf78bb8db6324d6af74fb8741bd1
mpaauw <matt.paauw@gmail.com>
2017-11-06 09:16:24 -0800

    Added validator interface and implementation.

---
+++ splash-rss/splash-rss/IValidator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace splash_rss
+{
+    /// <summary>
+    /// Confirms whether or not a user-submitted feed contains RSS data.
+    /// </summary>
+    public interface IValidator
+    {
+        bool ValidateFeed(Feed item);
+    }
+}
