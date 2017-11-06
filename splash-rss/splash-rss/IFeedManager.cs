5a460460cf52f2e562a9a7ee376c0c2665b5b824
mpaauw <matt.paauw@gmail.com>
2017-11-06 10:12:53 -0800

    Added a feed manager interface / implementation used to manage feed data.

---
+++ splash-rss/splash-rss/IFeedManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel.Syndication;
+
+namespace splash_rss
+{
+    public interface IFeedManager
+    {
+        List<SyndicationItem> LoadFeedData(Feed feed);
+    }
+}
