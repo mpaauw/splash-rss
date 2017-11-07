3d2f8ffebb79908ad00bae4d0431ceb5e8768fc6
mpaauw <matt.paauw@gmail.com>
2017-11-06 17:54:38 -0800

    Added a console manager interface / implementation pair to be used for navigating through the printable console.

---
+++ splash-rss/splash-rss/ConsoleManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel.Syndication;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace splash_rss
+{
+    public class ConsoleManager : IConsoleManager
+    {
+        public void Navigate(List<SyndicationItem> data)
+        {
+            
+        }
+
+        public void PrintData(List<SyndicationItem> data)
+        {
+            foreach(SyndicationItem item in data)
+            {
+                Console.WriteLine(item.Title.Text);
+            }
+        }
+    }
+}
