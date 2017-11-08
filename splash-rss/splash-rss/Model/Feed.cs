720fbc242678192270c0a47d748cc2ce039764bc
mpaauw <matt.paauw@gmail.com>
2017-11-07 17:49:54 -0800

    Added a new namespace for object based data model classes.

---
+++ splash-rss/splash-rss/Model/Feed.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace splash_rss.Model
+{
+    public class Feed
+    {
+        public Feed() { }
+
+        public Feed(string name, string endpoint)
+        {
+            this.name = name;
+            this.endpoint = endpoint;
+        }
+
+        public string name { get; set; }
+
+        public string endpoint { get; set; }
+    }
+}
