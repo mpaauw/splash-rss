b718a87df629fa2cfe688d24bff3c14ce6973f4b
mpaauw <matt.paauw@gmail.com>
2017-11-07 17:49:27 -0800

    Created a new namespace for display / console-related classes.

---
+++ splash-rss/splash-rss/Interface/IConsoleManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel.Syndication;
+
+namespace splash_rss.Interface
+{
+    public interface IConsoleManager
+    {
+        void Navigate(List<SyndicationItem> data);
+
+        void PrintData(List<SyndicationItem> data);
+    }
+}
