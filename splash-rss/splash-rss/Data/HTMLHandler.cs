8e68674a436fa8bf108cbd7862f97afec4c4c771
mpaauw <matt.paauw@gmail.com>
2017-11-09 17:48:55 -0800

    Added initial HTML retrieval methods to HTMLHandler interface and implementation.

--- splash-rss/splash-rss/Data/HTMLHandler.cs
+++ splash-rss/splash-rss/Data/HTMLHandler.cs
@@ -3,11 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HtmlAgilityPack;
 
 namespace splash_rss.Data
 {
     public class HTMLHandler : IHTMLHandler
     {
-
+        public HtmlDocument LoadHTMLDocumentFromWeb(string url)
+        {
+            HtmlWeb web = new HtmlWeb();
+            HtmlDocument doc = web.Load(url);
+            return doc;
+        }
     }
 }
--- splash-rss/splash-rss/Data/IHTMLHandler.cs
+++ splash-rss/splash-rss/Data/IHTMLHandler.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HtmlAgilityPack;
 
 namespace splash_rss.Data
 {
     public interface IHTMLHandler
     {
-
+        HtmlDocument LoadHTMLDocumentFromWeb(string url);
     }
 }
