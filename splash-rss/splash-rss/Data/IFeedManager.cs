88551bdbafe3bada6a24445eaa043408f2c45f77
mpaauw <matt.paauw@gmail.com>
2017-11-14 16:33:27 -0800

    Added documentation comments to IFeedManager interface.

--- splash-rss/splash-rss/Data/IFeedManager.cs
+++ splash-rss/splash-rss/Data/IFeedManager.cs
@@ -10,6 +10,11 @@ namespace splash_rss.Data
 {
     public interface IFeedManager
     {
+        /// <summary>
+        /// Loads in SyndicationItem data objects from a verified RSS feed.
+        /// </summary>
+        /// <param name="feed">The Feed object from which to load syndication data.</param>
+        /// <returns>A List of all SyndicationItem objects retrieved from the URL endpoint specified within the Feed object.</returns>
         List<SyndicationItem> LoadFeedData(Feed feed);
     }
 }
