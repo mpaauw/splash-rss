e60149f524d4f9ed689bdc4fdff60bf8c30554a0
mpaauw <matt.paauw@gmail.com>
2017-11-15 19:38:36 -0800

    Added HAP sample code to assist with html parsing.

--- splash-rss/splash-rss/Data/HTMLHandler.cs
+++ splash-rss/splash-rss/Data/HTMLHandler.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using HtmlAgilityPack;
+using System.IO;
 
 namespace splash_rss.Data
 {
@@ -23,6 +24,11 @@ namespace splash_rss.Data
 
         public string ParseHTMLDocument(HtmlDocument doc)
         {
+            string bodyHtml = doc.DocumentNode.SelectSingleNode(xBody).InnerHtml;
+
+            HtmlToText h2t = new HtmlToText();
+            var text = h2t.ConvertHtml(bodyHtml);
+
             // headings:
             foreach(string heading in xHeadings)
             {
@@ -67,6 +73,93 @@ namespace splash_rss.Data
 
             
             return doc.DocumentNode.SelectSingleNode(xBody).InnerText;
+        }
+    }
+
+    public class HtmlToText
+    {
+        public HtmlToText()
+        {
+        }
+
+        public string Convert(string path)
+        {
+            HtmlDocument doc = new HtmlDocument();
+            doc.Load(path);
+
+            StringWriter sw = new StringWriter();
+            ConvertTo(doc.DocumentNode, sw);
+            sw.Flush();
+            return sw.ToString();
+        }
+
+        public string ConvertHtml(string html)
+        {
+            HtmlDocument doc = new HtmlDocument();
+            doc.LoadHtml(html);
+
+            StringWriter sw = new StringWriter();
+            ConvertTo(doc.DocumentNode, sw);
+            sw.Flush();
+            return sw.ToString();
+        }
+
+        private void ConvertContentTo(HtmlNode node, TextWriter outText)
+        {
+            foreach (HtmlNode subnode in node.ChildNodes)
+            {
+                ConvertTo(subnode, outText);
+            }
+        }
+
+        public void ConvertTo(HtmlNode node, TextWriter outText)
+        {
+            string html;
+            switch (node.NodeType)
+            {
+                case HtmlNodeType.Comment:
+                    // don't output comments
+                    break;
+
+                case HtmlNodeType.Document:
+                    ConvertContentTo(node, outText);
+                    break;
+
+                case HtmlNodeType.Text:
+                    // script and style must not be output
+                    string parentName = node.ParentNode.Name;
+                    if ((parentName == "script") || (parentName == "style"))
+                        break;
+
+                    // get text
+                    html = ((HtmlTextNode)node).Text;
+
+                    // is it in fact a special closing node output as text?
+                    if (HtmlNode.IsOverlappedClosingElement(html))
+                        break;
+
+                    // check the text is meaningful and not a bunch of whitespaces
+                    if (html.Trim().Length > 0)
+                    {
+                        outText.Write(HtmlEntity.DeEntitize(html));
+                    }
+                    break;
+
+                case HtmlNodeType.Element:
+                    switch (node.Name)
+                    {
+                        case "p":
+                            // treat paragraphs as crlf
+                            outText.Write("\r\n");
+                            break;
+                    }
+
+                    if (node.HasChildNodes)
+                    {
+                        ConvertContentTo(node, outText);
+                    }
+                    break;
+            }
         }
     }
 }
