ee5f8b2c0645e44078f47a058599ad087b7ac971
mpaauw <matt.paauw@gmail.com>
2017-11-15 19:08:18 -0800

    Continued development of experimental test code within HTML handler classes.

--- splash-rss/splash-rss/Data/HTMLHandler.cs
+++ splash-rss/splash-rss/Data/HTMLHandler.cs
@@ -9,6 +9,11 @@ namespace splash_rss.Data
 {
     public class HTMLHandler : IHTMLHandler
     {
+        const string xBody = @"//body";
+        string[] xHeadings = { @"/h1", @"/h2", @"/h3", @"/h4", @"/h5", @"/h6" };
+        const string xParagraph = @"/p";
+        const string xDiv = @"/div";
+
         public HtmlDocument LoadHTMLDocumentFromWeb(string url)
         {
             HtmlWeb web = new HtmlWeb();
@@ -16,9 +21,52 @@ namespace splash_rss.Data
             return doc;
         }
 
-        public string ParseHTMLDocument(HtmlDocument doc, string nodeSelector = "//body")
+        public string ParseHTMLDocument(HtmlDocument doc)
         {
-            return doc.DocumentNode.SelectSingleNode(nodeSelector).InnerText;
+            // headings:
+            foreach(string heading in xHeadings)
+            {
+                Console.WriteLine("Heading: [" + heading + "]");
+
+                string nodeSelector = xBody + heading;
+                try
+                {
+                    foreach (HtmlNode node in doc.DocumentNode.SelectNodes(nodeSelector))
+                    {
+                        Console.WriteLine("[" + nodeSelector + "]: " + node.InnerText);
+                    }
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine("Selector not found: [" + nodeSelector + "]");
+                }
+
+                Console.WriteLine();
+                Console.ReadLine();
+            }
+
+            // paragraphs:
+            string selecta = xBody + xDiv;
+            try
+            {
+                foreach(HtmlNode node in doc.DocumentNode.SelectNodes(selecta))
+                {
+                    Console.WriteLine("[" + selecta + "]: " + node.InnerText);
+
+                    Console.WriteLine();
+                    Console.ReadLine();
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Selecta not found: [" + selecta + "]");
+
+                Console.WriteLine();
+                Console.ReadLine();
+            }
+
+            
+            return doc.DocumentNode.SelectSingleNode(xBody).InnerText;
         }
     }
 }
